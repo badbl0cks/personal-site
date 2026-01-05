8d989ef36f8e18407f1b923d9c669bfb028f62e7
badbl0cks <4161747+badbl0cks@users.noreply.github.com>
2026-01-04 21:36:08 -0800

    new minimal retro theme, progress on parallax hero, refactor into semantic html, add htmx and alpine, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,25 @@
+import htmx from "astro-htmx";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import alpinejs from "@astrojs/alpinejs";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import bun from "@nurodev/astro-bun";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://badblocks.dev",
+  adapter: bun(),
+  output: "static",
+  integrations: [
+    alpinejs(),
+    partytown(),
+    sitemap(),
+    htmx(),
+    // sitemap({
+    //   filter: (page) =>
+    //     page !== "https://example.com/secret-vip-lounge-1/" &&
+    //     page !== "https://example.com/secret-vip-lounge-2/",
+    // }),
+  ],
+});
